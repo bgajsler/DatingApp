2339a334a1d03eb5759b2c52229566f1baf2fa5b
Boštjan Gajsler <bostjan.gajsler@gmail.com>
2018-10-29 14:36:48 +0100

    Removed sources

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -58,6 +58,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForRegisterDto userForLoginDto)
         {
+            throw new Exception("Computer says no!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
             if(userFromRepo == null)
                 return Unauthorized();            
