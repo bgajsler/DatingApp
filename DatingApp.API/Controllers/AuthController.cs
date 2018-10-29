f598fa95713642fd0a4e92a4b6d9b41d8d729e95
Boštjan Gajsler <bostjan.gajsler@gmail.com>
2018-10-29 15:41:33 +0100

    Added error handlers - global ones to.

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -58,8 +58,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForRegisterDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
             if(userFromRepo == null)
                 return Unauthorized();            
